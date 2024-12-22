cc47f26a9af2f0414f05f99768cac46716d7d9c6
Joseph Salcedo <joe.salcedo100@gmail.com>
2024-12-22 16:07:09 -0500

    Change titles

--- GuacosTracker3/Controllers/HomeController.cs
+++ GuacosTracker3/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using GuacosTracker3.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System.Diagnostics;
 
 namespace GuacosTracker3.Controllers
@@ -7,6 +8,30 @@ namespace GuacosTracker3.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private string _title = "Home";
+        private string _subtitle = "";
+
+        [ViewData]
+        public string Page
+        {
+            get
+            {
+                if (_subtitle != "")
+                {
+                    string title = string.Format("{0} - {1}", _title, _subtitle);
+                    return title;
+                }
+                return _title;
+            }
+
+            set { _title = value; }
+        }
+
+        public string Subtitle
+        {
+            get { return _subtitle; }
+            set { _subtitle = value; }
+        }
 
         public HomeController(ILogger<HomeController> logger)
         {
