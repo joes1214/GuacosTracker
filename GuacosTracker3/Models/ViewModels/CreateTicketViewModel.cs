7b66ecf9cc0b4116fee6cbe12aaffa5223a205af
Joseph Salcedo <joe.salcedo100@gmail.com>
2025-03-17 00:26:08 -0400

    Update viewmodel

--- GuacosTracker3/Models/ViewModels/CreateTicketViewModel.cs
+++ GuacosTracker3/Models/ViewModels/CreateTicketViewModel.cs
@@ -1,17 +1,24 @@
-﻿namespace GuacosTracker3.Models.ViewModels
+﻿using System.Security.Policy;
+
+namespace GuacosTracker3.Models.ViewModels
 {
     public class CreateTicketViewModel
     {
+        private readonly IConfiguration _configuration;
+
         public Ticket Ticket { get; set; } = new Ticket();
 
-        public int CustomerID { get; set; }
-        public string Description { get; set; }
+        public int? CustomerID { get; set; }
+        public string? Description { get; set; }
         public string? CustomerFName { get; set; }
         public string? CustomerLName { get; set; }
+        public string APIUrl { get; private set; }
 
-        public CreateTicketViewModel() { }
+        public CreateTicketViewModel(string url) {
+            APIUrl = url;
+        }
 
-        public CreateTicketViewModel(int customerID, string description, string customerFName, string customerLName)
+        public CreateTicketViewModel(int customerID, string description, string customerFName, string customerLName, string url) : this(url)
         {
             Ticket = new Ticket();
             Description = description;
@@ -20,7 +27,7 @@
             CustomerLName = customerLName;
         }
 
-        public CreateTicketViewModel(string title, string employeeID, string description, string status, int customerID, string customerFName, string customerLName)
+        public CreateTicketViewModel(string title, string employeeID, string description, string status, int customerID, string customerFName, string customerLName, string url) : this(url)
         {
             Ticket = new Ticket(title, employeeID, status, customerID);
             Description = description;
