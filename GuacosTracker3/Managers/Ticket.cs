990e5f24762cc5d4c94149409f6234a4f8f1fdc0
Joseph Salcedo <joe.salcedo100@gmail.com>
2025-09-17 17:08:03 -0400

    Add ticket manager

---
+++ GuacosTracker3/Managers/Ticket.cs
@@ -0,0 +1,56 @@
+﻿using GuacosTracker3.Models;
+using GuacosTracker3.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace GuacosTracker3.Managers
+{
+    public class Ticket(Models.Ticket ticket)
+    {
+        private readonly Models.Ticket? ticket = ticket;
+
+        public static Models.Ticket CreateTicket(string employeeId, string title, string status, Models.Customer customer)
+        {
+            return new Models.Ticket(title, employeeId, status, customer);
+        }
+
+        public Models.Ticket EditTicket(string title, string employeeId, bool isClosed)
+        {
+            if (this.ticket == null)
+            {
+                throw new TicketManagerError("Ticket is null");
+            }
+
+            Models.Ticket ticket = this.ticket;
+            ticket.Title = title;
+            // Update the ticket's history with the employee id
+            ticket.IsClosed = isClosed;
+
+            return ticket;
+        }
+
+        public Models.Note CreateNote(Guid ticketId, string employeeId, string description, string status)
+        {
+            return new Models.Note(ticketId, employeeId, description, status);
+        }
+
+        public Models.Ticket DeleteTicket()
+        {
+            if (this.ticket == null)
+            {
+                throw new TicketManagerError("Ticket is null");
+            }
+
+            Models.Ticket ticket = this.ticket;
+            ticket.IsClosed = true;
+
+            return ticket;
+        }
+    }
+
+    public class TicketManagerError : Exception
+    {
+        public TicketManagerError() : base() { }
+        public TicketManagerError(string message) : base(message) { }
+        public TicketManagerError(string message, Exception inner) : base(message, inner) { }
+    }
+}
